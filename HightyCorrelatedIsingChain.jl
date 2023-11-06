a88c8b41c349f94478c23f5c731a8d0df416031c
dev-aditya <adityadev.iisermohali@gmail.com>
2023-11-06 16:52:46 +0100

    Minor Edits

--- HightyCorrelatedIsingChain.jl
+++ HightyCorrelatedIsingChain.jl
@@ -35,12 +35,12 @@ HC_EIG_E, HC_EIG_V = eigenstates(dense(Hc))
 GLOB_EIG_E, GLOB_EIG_V = eigenstates(dense(H))
 PSI_EIG_E, PSI_EIG_V = eigenstates(dense(Hs))
 
-function Χ(E::Float64, t::Float64)
+function χ(E::Float64, t::Float64)
     return sum([exp(-im * (HC_EIG_E[i] - E) * t) * HC_EIG_V[i] for i in eachindex(HC_EIG_E)])
 end
 
 function φ_λ(t::Float64, E::Float64, Ψ::Ket)
-    clc_v = Χ(E, t)
+    clc_v = χ(E, t)
     clock_coeff_conj = [dagger(clc_v) * c_basis for c_basis in HC_EIG_V]
     state = PSI_EIG_V[1] * 0.0
     for i in eachindex(PSI_EIG_E)
@@ -73,8 +73,7 @@ for index in eachindex(GLOB_EIG_E)
         end
     end
     Ψ = GLOB_EIG_V[index]
-    ρ_red = ptrace(dm(Ψ), [i for i=2:N])
-    entan = real(entanglement_entropy(Ψ, [i for i=2:N]))
+    entan = real(entanglement_entropy(Ψ, [i for i=2:N]))/log(2)
     plot(T, abs.(c1), label=("Energy: " * string(GLOB_EIG_E[index])), legend=:right)
     plot!(T, abs.(c2), label=("Entangement: " * string(entan)), legend=:right, figsize=(19.20, 15.80))
     xlabel!("t")
