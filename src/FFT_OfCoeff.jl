a67ef47b1995854ff5dd801d149a1abdb90e6aa9
dev-aditya <adityadev.iisermohali@gmail.com>
2023-11-13 15:03:34 +0100

    Major edits
    1. Made plots more detailed

--- src/FFT_OfCoeff.jl
+++ src/FFT_OfCoeff.jl
@@ -2,12 +2,21 @@ include("FiniteQuantSystem.jl")
 using .FiniteQuantSystem
 using QuantumOptics
 using PyPlot
+using PyPlot
+PyPlot.rc("axes", grid=true)
+using LaTeXStrings
 using Base.Threads
 using Statistics
-using LaTeXStrings
 using DelimitedFiles
-using FFTW
+using FFTW # for fft
+#= using PyCall
+mpl = pyimport("matplotlib.pyplot")
+mpl.style.use("seaborn") =#
 
+#= 
+1. Import the required Hamiltonians 
+2. Don't forgert to update the titles and labels of the plots. 
+=#
 include("hamiltonian/powerLawCoupling.jl")
 
 HC_EIG_E_loc, HC_EIG_V_loc = eigenstates(dense(Hc));
@@ -32,6 +41,7 @@ outfile = "data/energy_degeneracy_$N-powerLawCoupling_$l.txt"
 writedlm(outfile, degeneracy, '\t')
 
 T = Base._linspace(0.0, 2 * π, 1000);
+freqs = fftfreq(length(T), T[2] - T[1]) |> fftshift;
 for index in eachindex(quant_system.GLOB_EIG_E)
     UpdateIndex(quant_system, index)
     global c1 = Vector{ComplexF64}(undef, length(T))
@@ -45,21 +55,44 @@ for index in eachindex(quant_system.GLOB_EIG_E)
         c2[i] = ϕ.data[2]
     end
     ene = quant_system.EΨ
-    c1 = abs2.(c1) .- mean(abs2.(c1))
-    #c2 = abs2.(c2) .- mean(abs2.(c2))
-    Y1 = fft(c1) |> fftshift
-    freqs1 = fftfreq(length(T), T[2] - T[1]) |> fftshift
+    c1_var = abs2.(c1) .- mean(abs2.(c1))
+    c2_var = abs2.(c2) .- mean(abs2.(c2))
+    Y1 = fft(c1_var) |> fftshift
     entan = real(entanglement_entropy(quant_system.Ψ, [i for i = 2:N])) / log(2)
-    fig, axs = subplots(2, 1, figsize=(20, 10),)
-    axs[1].plot(T, log.(c1))
-    axs[1].set_title("C1^2 coefficient", fontsize=15)
-    axs[1].set_xlabel("t", fontsize=15)
-    axs[1].set_ylabel(L"log(|c_1|^2 - \overline{|c_1|^2})", fontsize=15)
-    axs[2].plot(freqs1, log.(abs.(Y1)))
-    axs[2].set_title("Freq Spectrum", fontsize=15)
-    axs[2].set_xlabel("Freq(Hz)", fontsize=15)
-    axs[2].set_ylabel("log(Power)", fontsize=15)
-    fig.suptitle("Energy: $(ene) \n Entanglement: $(entan)", fontsize=15)
+    fig, axs = subplots(2, 2, figsize=(45, 27),)
+    ##==============
+    axs[1, 1].plot(T, abs2.(c1), label=L"|c_1|^2", color="red",)
+    axs[1, 1].plot(T, abs2.(c2), label=L"|c_2|^2", color="blue",)
+    axs[1, 1].set_title("Coefficients together", fontsize=25)
+    axs[1, 1].set_xlabel("T", fontsize=25)
+    axs[1, 1].set_ylabel(L"|c_1|^2, |c_2|^2", fontsize=25)
+    axs[1, 1].tick_params(axis="x", labelsize=18)  # Increase x-ticks font size
+    axs[1, 1].tick_params(axis="y", labelsize=18)  # Increase y-ticks font size
+    axs[1, 1].legend(fontsize=25)
+    ##======##======
+    fft_c1 = fft(abs2.(c1)) |> fftshift
+    axs[1, 2].plot(freqs, log.(abs.(fft_c1)), linestyle="-.", linewidth=1.2)
+    axs[1, 2].set_title(L"log(|FFT(|c_1|^2)|)", fontsize=25)
+    axs[1, 2].set_xlabel("Freq(Hz)", fontsize=25)
+    axs[1, 2].set_ylabel(L"log(|FFT(|c_1|^2)|)", fontsize=25)
+    axs[1, 2].tick_params(axis="x", labelsize=18)  # Increase x-ticks font size
+    axs[1, 2].tick_params(axis="y", labelsize=18)  # Increase y-ticks font size
+    ##======##======
+    axs[2, 1].plot(T, c1_var, linestyle="-.", linewidth=1.2)
+    axs[2, 1].set_title(L"|c_1|^2 - \overline{|c_1|^2}", fontsize=25)
+    axs[2, 1].set_xlabel("T", fontsize=25)
+    axs[2, 1].set_ylabel(L"var(|c_1|^2)", fontsize=25)
+    axs[2, 1].tick_params(axis="x", labelsize=14)  # Increase x-ticks font size
+    axs[2, 1].tick_params(axis="y", labelsize=14)  # Increase y-ticks font size
+    ##======##======
+    axs[2, 2].plot(freqs, log.(abs.(Y1)), linestyle="-.", linewidth=1.2)
+    axs[2, 2].set_title(L"log(|FFT(var(|c_1|^2))|)", fontsize=25)
+    axs[2, 2].set_xlabel("Freq(Hz)", fontsize=25)
+    axs[2, 2].set_ylabel(L"log(|FFT(var(|c_1|^2))|)", fontsize=25)
+    axs[2, 2].tick_params(axis="x", labelsize=18)  # Increase x-ticks font size
+    axs[2, 2].tick_params(axis="y", labelsize=18)  # Increase y-ticks font size
+
+    fig.suptitle("PowerLaw Coupling \n Energy: $(ene); Entanglement: $(entan); gamma = $γ", fontsize=30)
     PyPlot.savefig("data/index_$index-powerLaw.png",)
     PyPlot.close()
 end
